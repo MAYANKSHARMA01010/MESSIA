d06a85295ae90df361d1994c1968aa64f47ca3e5
Mayank Sharma <132637383+MAYANKSHARMA01010@users.noreply.github.com>
2025-11-02 02:49:12 +0530

    dummy

--- apps/backend/middlewares/authMiddleware.js
+++ apps/backend/middlewares/authMiddleware.js
@@ -1,42 +1,10 @@
 const { prisma } = require("../config/database");
 
 async function createUserMiddleware(req, res, next) {
-    let { name, username, email, password, confirm_password } = req.body;
 
-    if (!name || !password || !confirm_password || !email || !username) {
-        return res.status(400).json({
-            ERROR: "All fields are required",
-        });
-    }
-
-    email = email.toLowerCase();
-    username = username.toLowerCase();
-
-    try {
-        const existingUser = await prisma.user.findFirst({
-            where: {
-                OR: [{ email }, { username }],
-            },
-        });
-
-        if (existingUser) {
-            return res.status(400).json({
-                ERROR: "Email or Username already exists",
-            });
-        }
-
-        req.body.email = email;
-        req.body.username = username;
-        next();
-    } 
-    catch (err) {
-        console.error(err);
-        return res.status(500).json({
-            message: "Database not able to respond",
-        });
-    }
 }
 
 module.exports = { 
     createUserMiddleware 
 };
+
