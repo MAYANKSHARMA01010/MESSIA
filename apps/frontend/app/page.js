3d978c3ebf6a8fd393eab6f9bc3f6419f2d21bdf
Mayank Sharma <132637383+MAYANKSHARMA01010@users.noreply.github.com>
2025-11-12 18:50:45 +0530

    dummy

--- apps/frontend/app/page.js
+++ apps/frontend/app/page.js
@@ -1,9 +1,11 @@
+import HomeSection from "./components/LandingPage/HomeSection";
 import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      {/* <HomeSection /> */}
     </div>
   );
 }
