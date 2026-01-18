c2da01c28e82db1e950982d0023673d5ee150816
Mayank Sharma <132637383+MAYANKSHARMA01010@users.noreply.github.com>
2026-01-18 20:47:55 +0530

    minor changes

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+
+  basePath: "/MESSIA",
+  assetPrefix: "/MESSIA/",
+
   images: {
+    unoptimized: true,
     domains: [
       "picsum.photos",
       "placehold.co",
